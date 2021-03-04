8fef4bda0953b73c0237e9cdc5b53d572b5ba01d
123-dani <78221119+123-dani@users.noreply.github.com>
2021-03-04 07:04:47 -0500

    Add files via upload

---
+++ actividad 2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <windows.h>
+#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+	
+	int veses,conta=1;
+	string resu;
+	while(conta==1);{
+	cout<<"escriba pepe \n";
+	cout<<"GRACIAS";
+	cin>>resu;
+	if(resu=="pepe";{
+	return 0;
+}
+else {
+	cout<<"RIPETA LA OPERACION, ESTA MAL";
+	
+	
+}
+}
